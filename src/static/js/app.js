129a38837400e8fc10761f847ab909e2e31194a8
Ailani M Borges Lopez <aborgesl@charlotte.edu>
2026-04-21 16:51:36 -0400

    Change button text from 'Add Item' to 'Add One Item'

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add One Item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
